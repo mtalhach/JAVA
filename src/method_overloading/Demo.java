f944b822342ba1a36d13c74085bf52671e544a21
TALHA <talhashafiqch@gmail.com>
2022-03-31 15:49:09 +0500

    java Content

---
+++ src/method_overloading/Demo.java
@@ -0,0 +1,27 @@
+package method_overloading;
+
+public class Demo {
+    public static void main(String[] args) {
+        Main math=new Main();
+
+        System.out.println("Addition:");
+        int sum1=math.add(5,5);
+        double sum2=math.add(7.0,7.0);
+        double sum3=math.add(5,7.0);
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println("=============");
+
+        System.out.println("Multiplication:");
+        System.out.println(math.multiply(2,3));
+        System.out.println(math.multiply(3.7,4.5));
+        System.out.println(math.multiply(9,8.5));
+        System.out.println("=============");
+
+        System.out.println("Min Term:");
+        System.out.println(math.min(7,5));
+        System.out.println(math.min(8,9,11));
+        System.out.println(math.min(4,6,7,8));
+    }
+}
---
+++ src/method_overloading/Main.java
@@ -0,0 +1,44 @@
+package method_overloading;
+
+public class Main {
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+    public double add(double n1,double n2){
+        return n1+n2;
+    }
+
+    public int multiply(int n1,int n2){
+        return n1*n2;
+    }
+    public double multiply(double n1,double n2){
+        return n1*n2;
+    }
+
+    public int min(int x,int y){
+        if (x<y)
+            return x;
+        else
+            return y;
+    }
+    public int min(int x,int y,int z){
+        if (x<y&&x<z)
+            return x;
+        else if (y<z)
+            return y;
+        else
+            return z;
+    }
+    public int min(int w,int x,int y,int z){
+        if (w<x&&w<y&&w<z)
+            return w;
+        else if (x<y&&x<z)
+            return x;
+        else if (y<z)
+            return y;
+        else
+            return z;
+    }
+}
+
+
